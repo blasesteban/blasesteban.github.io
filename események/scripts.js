f6d52ec90830b6a81bee3bcb6ff29fdc1cf7ff89
blasesteban <blasesteban1@gmail.com>
2021-03-23 17:44:15 +0100

    g

---
+++ események/scripts.js
@@ -0,0 +1,9 @@
+$('#1').on('click', () => {
+    $(console).log(“Igen, most rámkattintottál”)
+});
+$('#2').on('click', () =>{
+    $('#1').text('baba')
+});
+$('#3').on('click', () =>{
+    $('button').css('background','purple')
+})
