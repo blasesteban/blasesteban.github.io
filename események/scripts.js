41808def6ed8f0c0fbfed26e764e9d58eee46a17
blasesteban <blasesteban1@gmail.com>
2021-03-23 21:01:10 +0100

    a

--- események/scripts.js
+++ események/scripts.js
@@ -1,9 +1,11 @@
 $('#1').on('click', () => {
-    $(console).log(“Igen, most rámkattintottál”)
-});
-$('#2').on('click', () =>{
+    console.log('Igen, most rámkattintottál')
+})
+$('#2').on('click', () => {
     $('#1').text('baba')
-});
-$('#3').on('click', () =>{
+})
+$('#3').on('click', () => {
+    console.log($('input').val())
     $('button').css('background','purple')
+    
 })
--- list-k-s-sz-vegek/list-k-s-sz-vegek/dist/scripts.js
+++ list-k-s-sz-vegek/list-k-s-sz-vegek/dist/scripts.js
@@ -1,14 +1,14 @@
 let nevek = ['Baba','Bebe','Bibi','Bobo']
-nevek.forEach(function (név) {
-$('ul').append('li')
-$('ul li:last-of-type').text(név)
+nevek.forEach((nam) => {
+$('ul').append(`<li>${nam}</li>`)
 })
 
 let additionalBlock = {
     title: "Beillesztve JavaScripttel",
     text: "Ez a blokk a JavaScript jQuery könyvtárával lett beillesztve. Király!"
-  };
-function() {
-    $('main').append('<h1>"title"</h1>')
-    $('main').append('<p>"text"</p>')
+}
+
+function ('a') {
+    $('main').append(`<h1>${title}</h1>`)
+    $('main').append(`<p>${text}</p>`)
 }
